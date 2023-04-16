52f8aa7ccf1007f632f58899b8999f66e4f4d303
Alexandr <mr.droidcaxa@mail.ru>
2023-04-16 21:03:56 +0700

    Change ge.c code

--- src/geometry/ge.c
+++ src/geometry/ge.c
@@ -1,25 +1,29 @@
-#include "libgeometry/ge_calc.h"
+#include "../libgeometry/ge_calc.h"
 #include <stdio.h>
 #include <string.h>
 
 #define MAX_INPUT_LENGTH 100
 
-int main() {
+int main()
+{
     char input[MAX_INPUT_LENGTH];
-    float x, y, radius,area,perimeter;
+    float x, y, radius;
 
     while (1) {
         printf("Input 'circle(x y, radius)': ");
         fgets(input, MAX_INPUT_LENGTH, stdin);
         if (sscanf(input, "circle(%f %f, %f)", &x, &y, &radius) == 3) {
             printf("x,y: (%.2f, %.2f)\n", x, y);
-            printf("Radius: %.2f\n", radius);
+            printf("Radius: %.2f", radius);
             break;
         } else {
             printf("Error. Try again\n");
+            return 0;
         }
     }
-    area = circle_area(radius);
-    perimeter = circle_perimeter(radius);
+    float area = circle_area(radius);
+    float perimeter = circle_perimeter(radius);
+    printf("\nArea: %f", area);
+    printf("\nPerimeter: %f\n", perimeter);
     return 0;
 }
