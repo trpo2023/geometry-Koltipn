bb431c8b4b8a48bec43f47048291a78bf4939baa
Alexandr <mr.droidcaxa@mail.ru>
2023-04-24 02:57:16 +0700

    Add test library

---
+++ test/main.c
@@ -0,0 +1,10 @@
+#define CTEST_MAIN
+#define CTEST_COLOR_OK
+#define CTEST_SEGFAULT
+
+#include "../thirdparty/ctest.h"
+
+int main(int argc, const char** argv)
+{
+    return ctest_main(argc, argv);
+}
