f95c2511ae5168e70f68a89e15e57702205e20f0
Alexandr <mr.droidcaxa@mail.ru>
2023-04-24 03:07:47 +0700

    Fix codestyle in ge_calc.c

--- src/libgeometry/ge_calc.c
+++ src/libgeometry/ge_calc.c
@@ -3,20 +3,18 @@
 
 float circle_area(float r)
 {
-    if(r > 0){
-    return M_PI * r * r;
-    }
-    else{
-    return 0;
+    if (r > 0) {
+        return M_PI * r * r;
+    } else {
+        return 0;
     }
 }
 
 float circle_perimeter(float r)
 {
-    if(r > 0){
-    return 2 * M_PI * r;
-    }
-    else{
-    return 0;
+    if (r > 0) {
+        return 2 * M_PI * r;
+    } else {
+        return 0;
     }
 }
